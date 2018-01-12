4b851bef28a84bc22430a556e00c5472ff2ba79b
quigleyjacob <31415768+quigleyjacob@users.noreply.github.com>
2018-01-11 22:22:08 -0600

    Style about page to include link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,11 @@ var React = require("react");
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
+            <h1 className="text-center">About</h1>
             <p>Welcome to the about page!</p>
+            <p>This is an experimental app to get used to uing React.</p>
+            <p>It is created using <a href="https://foundation.zurb.com/" target="_blank">Foundation</a> and <a href="https://openweathermap.org/" target="_blank">Open Weather Map</a></p>
+            <p>Click <a href="https://github.com/quigleyjacob/WeatherReact target="_blank>here</a> to visit the GitHub repository.</p>
         </div>
     );
 }
